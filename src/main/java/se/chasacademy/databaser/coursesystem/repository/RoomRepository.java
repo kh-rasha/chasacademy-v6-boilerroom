f74f756bb4553191903e1692167a9b95797a2870
kh-rasha <rasha.alkhateeb97@gmail.com>
2025-12-16 09:30:10 +0100

    updatera RoomRepository class.

--- src/main/java/se/chasacademy/databaser/coursesystem/repository/RoomRepository.java
+++ src/main/java/se/chasacademy/databaser/coursesystem/repository/RoomRepository.java
@@ -1,4 +1,12 @@
 package se.chasacademy.databaser.coursesystem.repository;
 
-public interface RoomRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import se.chasacademy.databaser.coursesystem.model.Room;
+
+import java.util.List;
+
+public interface RoomRepository extends JpaRepository<Room, Long> {
+
+    // 5) Rooms where capacity > X
+    List<Room> findByCapacityGreaterThan(int capacity);
 }
